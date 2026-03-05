27966d7513081e63efbc14a2fc7292e4f4ac550d
André Viezzer <viezzerbrasil@gmail.com>
2026-03-05 10:13:08 -0300

    Create AppColor.kt

---
+++ AppColor.kt
@@ -0,0 +1,38 @@
+package com.conelly.ads.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ConellyLime = Color(0xFFAECF36)       
+val ConellyLimeLight = Color(0xFFD4E885) 
+val ConellyRust = Color(0xFF784537)       
+
+val BrandGoogle = Color(0xFF4285F4)
+val BrandMeta = Color(0xFF0668E1)
+val BrandWhatsApp = Color(0xFF25D366)
+
+val ConellyDarkBackground = Color(0xFF05080D) 
+val ConellyDarkSurface = Color(0xFF141C29)    
+
+val ConellyDarkGlass = Color(0xFF1C2536).copy(alpha = 0.60f) 
+val ConellyDarkGlassBorder = Color(0xFFFFFFFF).copy(alpha = 0.08f) 
+val ConellyDarkTextPrimary = Color(0xFFF9FAFB)
+val ConellyDarkTextSecondary = Color(0xFF9CA3AF) 
+
+val ConellyLightBackground = Color(0xFFF2F4F7) 
+val ConellyLightSurface = Color(0xFFFFFFFF)    
+
+val ConellyLightGlass = Color(0xFFFFFFFF).copy(alpha = 0.75f) 
+val ConellyLightGlassBorder = Color(0xFF000000).copy(alpha = 0.05f)
+val ConellyLightTextPrimary = Color(0xFF111827) 
+val ConellyLightTextSecondary = Color(0xFF4B5563) 
+
+val ConellyOrange = ConellyLime
+val ConellyOrangeDark = ConellyRust
+val ConellyBlue = BrandGoogle
+val ConellyGreen = BrandWhatsApp
+
+val AppBackground = ConellyDarkBackground
+val AppCard = ConellyDarkSurface
+val AppText = ConellyDarkTextPrimary
+val AppFieldBackground = ConellyDarkSurface
+val AppTextSecondary = ConellyDarkTextSecondary
